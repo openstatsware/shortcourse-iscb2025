709687ec2f4259bc53217198212de31581e94fd5
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2024-04-02 22:14:01 +0200

    add installation script

---
+++ slides/examples/install.r
@@ -0,0 +1,25 @@
+install.packages(c(
+    "tidyverse",
+    "testthat",
+    "covr",
+    "shiny",
+    "shinytest2",
+    "profvis",
+    "data.table",
+    "usethis",
+    "roxygen2",
+    "devtools",
+    "rlang",
+    "checkmate",
+    "knitr",
+    "rmarkdown",
+    "glue",
+    "httr",
+    "styler",
+    "lintr",
+    "pkgdown",
+    "remotes"
+))
+
+# Install using the remotes package
+remotes::install_github("rstudio/shinyuieditor")

8372026faa67436932c691f6194835eaa6862434
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2024-04-07 21:50:28 +0200

    adapt last shiny script

--- slides/examples/shiny/AppDriver.R
+++ slides/examples/shiny/AppDriver.R
@@ -1,34 +1,34 @@
-library(shinytest)
-packageVersion("shinytest")
+library(shinytest2)
+packageVersion("shinytest2")
 
 # Basic operation.
-app <- ShinyDriver$new("basic/")
-app$setInputs(name = "Daniel")
-app$getValue("greeting")
+app <- AppDriver$new("basic")
+app$set_inputs(name = "Daniel")
+app$get_value(output = "greeting")
 app$click("reset")
-app$getValue("greeting")
-app$takeScreenshot()
+app$get_value(output = "greeting")
+app$get_screenshot()
 
 # Can use then together with testthat.
 library(testthat)
 # Even better is to make sure to wait long enough.
-app$setInputs(name = "Daniel")
-value1 <- app$waitForValue("greeting", iotype = "output")
+app$set_inputs(name = "Daniel")
+value1 <- app$wait_for_value(output = "greeting")
 expect_identical(value1, "Hi Daniel")
-app$setInputs(name = "Ben")
+app$set_inputs(name = "Ben")
 # Make sure that greeting has been changed from old value before we take it.
-value2 <- app$waitForValue("greeting", ignore = list(value1), iotype = "output")
+value2 <- app$wait_for_value(output = "greeting", ignore = list(value1))
 expect_identical(value2, "Hi Ben")
 app$click("reset")
 # Same idea here.
-value3 <- app$waitForValue("greeting", ignore = list(value2), iotype = "output")
+value3 <- app$wait_for_value(output = "greeting", ignore = list(value2))
 expect_identical(value3, "")
 # Oops - it is not! Why?
 value3
 # So we can instead e.g. expect this:
 expect_identical(value3$message, "")
-app$takeScreenshot()
-app$getAllValues()
+app$get_screenshot()
+app$get_values()
 
 # For Help:
-?ShinyDriver
+?AppDriver

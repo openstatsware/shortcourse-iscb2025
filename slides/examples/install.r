6e020c60390f9d24894c582f389a1c373a115a79
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2024-04-07 21:13:13 +0200

    fix index, 1, 2, 3

--- slides/examples/install.r
+++ slides/examples/install.r
@@ -23,3 +23,9 @@ install.packages(c(
 
 # Install using the remotes package
 remotes::install_github("rstudio/shinyuieditor")
+# For this package, please see https://rstudio.github.io/shinyuieditor/
+# for Installation instructions and
+# https://rstudio.github.io/shinyuieditor/#trouble-installing
+# if you have trouble with the installation.
+# It is not super essential for the workshop
+# so no worries if it does not install in the end.
